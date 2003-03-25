91b59acb0764ed376be99651ea87e154855e0636
Eh Tan <tan2@earth.sinica.edu.tw>
2003-03-25 05:48:45 +0000

    1. change attribute 'R0' to 'outer_radius' 2. outer_radius, inRadius, and inDepth are read-only now 3. add self-test section

--- pyre/RadiusDepth.py
+++ pyre/RadiusDepth.py
@@ -11,7 +11,17 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 """Classes supporting conversion of depth and radius on the fly.
+
+class:
+    Radius(radius, outer_radius=1)
+    Depth(depth, outer_radius=1)
+
+    both classes have 3 attributes:
+      outer_radius, inRadius, inDepth
+
+
 <usage example:>
+
 import RadiusDepth
 
 earth_radius = 6371
@@ -32,47 +42,91 @@ cmb.inRadius
 class RadiusDepthBase(object):
 
     def __init__(self, outer_radius):
-	self.R0 = outer_radius
+	self.Ro = outer_radius
+
+    def get_outer_radius(self):
+	return self.Ro
+
+    outer_radius = property(get_outer_radius, None, None,
+			    "outer-radius, read-only")
 
 
 
 class Radius(RadiusDepthBase):
 
-    def __init__(self, radius, outer_radius):
+    def __init__(self, radius, outer_radius=1):
 	if not 0 <= radius <= outer_radius:
 	    raise ValueError, "radius out of range (0 - outer_radius)"
 	RadiusDepthBase.__init__(self, outer_radius)
-	self.inRadius = radius
+	self.radius = radius    
+
+    def get_inRadius(self):
+	return self.radius
 
     def get_inDepth(self):
-	return self.R0 - self.inRadius
+	return self.outer_radius - self.radius
+
+    inRadius = property(get_inRadius, None, None,
+			"radius, read-only")
 
     inDepth = property(get_inDepth, None, None,
 		       "convert radius to depth, read-only")
 
-    __slots__ = ("R0", "inRadius", "inDepth")
-
+    __slots__ = ('inRadius', 'inDepth')
 
 
 class Depth(RadiusDepthBase):
 
-    def __init__(self, depth, outer_radius):
+    def __init__(self, depth, outer_radius=1):
 	if not 0 <= depth <= outer_radius:
 	    raise ValueError, "depth out of range (0 - outer_radius)"
 	RadiusDepthBase.__init__(self, outer_radius)
-	self.inDepth = depth
+	self.depth = depth
 
     def get_inRadius(self):
-	return self.R0 - self.inDepth
+	return self.outer_radius - self.depth
+
+    def get_inDepth(self):
+	return self.depth
 
     inRadius = property(get_inRadius, None, None,
 			"convert depth to radius, read-only")
 
-    __slots__ = ("R0", "inRadius", "inDepth")
+    inDepth = property(get_inDepth, None, None,
+		       "depth, read-only")
 
+
+####################################################
+
+if __name__ == "__main__":
+
+    print dir()
+    
+    earth_radius = 6371
+    phase660 = Depth(660, earth_radius)
+    
+    # this will report depth of phase660
+    print "in depth:", phase660.inDepth
+    
+    # this will report radius of phase660
+    print "in radius:",  phase660.inRadius
+
+    try:
+	# this will raise an exception
+	phase660.inRadius = 100
+    except:
+	print "exception catched"
+
+    print dir(phase660)
+
+    
+    cmb = Radius(3480, earth_radius)
+    print cmb.inDepth
+    print cmb.inRadius
+    print dir(cmb)
 
 
 # version
-__id__ = "$Id: RadiusDepth.py,v 1.1 2003/03/24 19:48:11 tan2 Exp $"
+__id__ = "$Id: RadiusDepth.py,v 1.2 2003/03/24 21:48:45 tan2 Exp $"
 
 # End of file 
