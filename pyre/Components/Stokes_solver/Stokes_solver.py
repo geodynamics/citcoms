8cf4b24961edd4f029d3eb9a70a7c2096b5f4bc9
Eh Tan <tan2@earth.sinica.edu.tw>
2003-05-15 07:07:54 +0000

    Initial revision

---
+++ pyre/Components/Stokes_solver/Stokes_solver.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                             Michael A.G. Aivazis
+#                      California Institute of Technology
+#                      (C) 1998-2003  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+from mpi.Application import Application
+
+class Stokes_solver(Application):
+
+
+    def main(self):
+	# Solver doesn't run stand along
+	return
+
+    def run(self, *args, **kwds):
+	Application.run(*args, **kwds)
+	return
+
+
+    def output(self, *args, **kwds):
+	return
+
+
+    def __init__(self, name):
+	Application.__init__(self, name)
+	return
+
+
+# version
+__id__ = "$Id: Stokes_solver.py,v 1.1 2003/05/15 00:07:54 tan2 Exp $"
+
+# End of file 
---
+++ pyre/Components/Stokes_solver/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                             Michael A.G. Aivazis
+#                      California Institute of Technology
+#                      (C) 1998-2003  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+
+def imcompressible():
+    from Imcompressible import Imcompressible
+    return Imcompressible()
+    
+
+
+
+
+
+
+
+
+
+# version
+__id__ = "$Id: __init__.py,v 1.1 2003/05/15 00:07:54 tan2 Exp $"
+
+# End of file 
