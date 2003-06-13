2cd06bcd6e5544be60f690daa4fe8611ab10b726
Eh Tan <tan2@earth.sinica.edu.tw>
2003-06-13 01:25:14 +0000

    1. remove 'license_text' 2. add Common/Component/__init__.py

--- pyre/Components/__init__.py
+++ pyre/Components/__init__.py
@@ -7,11 +7,9 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 
 
-def copyright():
-    return "CitcomS pyre module: Copyright (c) 1998-2003 California Institute of Technology";
 
 
 # version
-__id__ = "$Id: __init__.py,v 1.2 2003/06/12 18:25:14 tan2 Exp $"
+__id__ = "$Id: __init__.py,v 1.1 2003/06/12 18:25:14 tan2 Exp $"
 
 #  End of file 
