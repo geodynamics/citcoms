6fd454cebdb2c2d6a9391c82bc876e08e044064d
Eh Tan <tan2@earth.sinica.edu.tw>
2003-08-16 01:59:58 +0000

    *** empty log message ***

--- lib/Common/drive_solvers.h
+++ lib/Common/drive_solvers.h
@@ -1,5 +1,2 @@
-void general_stokes_solver_init(struct All_variables*);
-void general_stokes_solver_fini(struct All_variables*);
-void general_stokes_solver_update_velo(struct All_variables*);
-void general_stokes_solver_Unorm(struct All_variables*, double*, double*);
-void general_stokes_solver_log(struct All_variables*, float, float, int);
+void general_stokes_solver(struct All_variables*);
+void general_stokes_solver_setup(struct All_variables*);
