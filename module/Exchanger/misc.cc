dfaf409329ecb07742907224a120735304d1ba39
Eh Tan <tan2@earth.sinica.edu.tw>
2003-09-12 04:41:20 +0000

    add All_variables data, which is required by Boundary object

--- module/Exchanger/misc.cc
+++ module/Exchanger/misc.cc
@@ -9,6 +9,7 @@
 
 #include <portinfo>
 #include <Python.h>
+#include <iostream>
 
 extern "C" {
 #include "global_defs.h"
@@ -51,12 +52,49 @@ PyObject * pyExchanger_returnE(PyObject *, PyObject *)
     All_variables *E = new All_variables;
 
     E->parallel.me = 1;
+    E->parallel.me_loc[1] = 0;
+    E->parallel.me_loc[2] = 0;
+    E->parallel.me_loc[3] = 0;
+    E->parallel.nprocx = 1;
+    E->parallel.nprocy = 1;
+    E->parallel.nprocz = 1;
+
+    E->sphere.caps_per_proc = 1;
+
+    E->mesh.levmax = 1;
+    E->mesh.dof = 3;
+    E->mesh.nox = E->lmesh.nox = 4;
+    E->mesh.noy = E->lmesh.noy = 4;
+    E->mesh.noz = E->lmesh.noz = 3;
+
+    const int n = E->lmesh.nox * E->lmesh.noy * E->lmesh.noz;
+    for(int m=1;m<=E->sphere.caps_per_proc;m++)
+	for(int i=1; i<=E->mesh.dof; i++) {
+	    E->X[E->mesh.levmax][m][i] = new double[n+1];
+    }
+
+    for(int m=1;m<=E->sphere.caps_per_proc;m++)
+	for(int k=1;k<=E->lmesh.noy;k++)
+	    for(int j=1;j<=E->lmesh.nox;j++)
+		for(int i=1;i<=E->lmesh.noz;i++)  {
+		    int node = i + (j-1)*E->lmesh.noz
+			     + (k-1)*E->lmesh.noz*E->lmesh.nox;
+		    E->X[E->mesh.levmax][m][1][node] = j;
+		    E->X[E->mesh.levmax][m][2][node] = k;
+		    E->X[E->mesh.levmax][m][3][node] = i;
+
+//  		    std::cout << node << " "
+//  			      << E->X[E->mesh.levmax][m][1][node] << " "
+//  			      << E->X[E->mesh.levmax][m][2][node] << " "
+//  			      << E->X[E->mesh.levmax][m][3][node] << " "
+//  			      << std::endl;
+		}
 
     PyObject *cobj = PyCObject_FromVoidPtr(E, NULL);
     return Py_BuildValue("O", cobj);
 }
 
 // version
-// $Id: misc.cc,v 1.3 2003/09/09 20:57:25 tan2 Exp $
+// $Id: misc.cc,v 1.4 2003/09/11 21:41:20 tan2 Exp $
 
 // End of file
