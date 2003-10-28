8239db1dfb6d862410f178988a1cb97b99493b7c
Eh Tan <tan2@earth.sinica.edu.tw>
2003-10-28 09:55:01 +0000

    add default component name

--- pyre/Components/Sphere/__init__.py
+++ pyre/Components/Sphere/__init__.py
@@ -9,19 +9,19 @@
 
 
 
-def fullSphere(name, facility='mesher'):
+def fullSphere(name="full-sphere", facility='mesher'):
     from FullSphere import FullSphere
     return FullSphere(name, facility)
 
 
 
-def regionalSphere(name, facility='mesher'):
+def regionalSphere(name="regional-sphere", facility='mesher'):
     from RegionalSphere import RegionalSphere
     return RegionalSphere(name, facility)
 
 
 
 # version
-__id__ = "$Id: __init__.py,v 1.4 2003/08/27 20:52:47 tan2 Exp $"
+__id__ = "$Id: __init__.py,v 1.5 2003/10/28 01:55:01 tan2 Exp $"
 
 # End of file
