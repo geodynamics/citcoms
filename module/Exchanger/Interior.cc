252105b62a377e67f4a4a50fbabc45bffbc3257b
Puru Thoutireddy <puru@cacr.caltech.edu>
2003-11-07 13:47:16 +0000

    fixed the bug

--- module/Exchanger/Interior.cc
+++ module/Exchanger/Interior.cc
@@ -8,6 +8,9 @@
 //
 
 #include <portinfo>
+#include <algorithm>
+#include <limits>
+#include <vector>
 #include "global_defs.h"
 #include "BoundedBox.h"
 #include "Interior.h"
@@ -22,6 +25,21 @@ Interior::Interior() :
 Interior::Interior(const BoundedBox& remoteBBox, const All_variables* E) :
     BoundedMesh(E)
 {
+    std::vector<double> x(DIM);
+    int interiornodes,node;
+    interiornodes=0;
+
+    for (int m=1;m<=E->sphere.caps_per_proc;m++)
+        for(int i=1;i<=E->lmesh.nox;i++) 
+	    for(int j=1;j<=E->lmesh.noy;j++)
+		for(int k=1;k<=E->lmesh.noz;k++)   
+                {
+                    interiornodes++;
+                }
+
+    X_.reserve(interiornodes);
+    meshID_.reserve(interiornodes);
+    
     for (int m=1;m<=E->sphere.caps_per_proc;m++)
         for(int i=1;i<=E->lmesh.nox;i++) 
 	    for(int j=1;j<=E->lmesh.noy;j++)
@@ -35,10 +53,16 @@ Interior::Interior(const BoundedBox& remoteBBox, const All_variables* E) :
                        (E->sx[m][3][node]> remoteBBox[0][2]) &&
                        (E->sx[m][3][node]< remoteBBox[1][2]))
                     {
-                       appendX(E, m, node);
+                        for(int k=0; k<DIM; k++)
+                            x[k] = E->sx[m][k+1][node];
+                        X_.push_back(x);
+                        meshID_.push_back(node);
                     }                    
                 }
-    
+    X_.shrink();
+    X_.print("X");
+    meshID_.shrink();
+    meshID_.print("meshID");
 }
 
 
@@ -50,6 +74,6 @@ void Interior::broadcast(const MPI_Comm& comm, int broadcaster)
 
 
 // version
-// $Id: Interior.cc,v 1.2 2003/11/07 05:14:16 puru Exp $
+// $Id: Interior.cc,v 1.3 2003/11/07 05:47:16 puru Exp $
 
 // End of file
