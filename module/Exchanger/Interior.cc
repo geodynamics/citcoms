24cc2403f68508787c8f47787d4cdcbab667ea8a
Puru Thoutireddy <puru@cacr.caltech.edu>
2003-11-07 13:14:16 +0000

    Set interior nodes for two way communication

--- module/Exchanger/Interior.cc
+++ module/Exchanger/Interior.cc
@@ -22,48 +22,23 @@ Interior::Interior() :
 Interior::Interior(const BoundedBox& remoteBBox, const All_variables* E) :
     BoundedMesh(E)
 {
-
-#if 0
-    int node,n,l;
-
-    int size_=0;
     for (int m=1;m<=E->sphere.caps_per_proc;m++)
-        for(int i=1;i<=E->lmesh.nox;i++)
+        for(int i=1;i<=E->lmesh.nox;i++) 
 	    for(int j=1;j<=E->lmesh.noy;j++)
-		for(int k=1;k<=E->lmesh.noz;k++)
+		for(int k=1;k<=E->lmesh.noz;k++)   
                 {
                     node = k + (i-1)*E->lmesh.noz+(j-1)*E->lmesh.nox*E->lmesh.noz;
-                    if((E->sx[m][1][node]> b->theta_min()) &&
-                       (E->sx[m][1][node]< b->theta_max()) &&
-                       (E->sx[m][2][node]> b->fi_min()) &&
-                       (E->sx[m][2][node]< b->fi_max()) &&
-                       (E->sx[m][3][node]> b->ri()) &&
-                       (E->sx[m][3][node]< b->ro()))
+                    if((E->sx[m][1][node]> remoteBBox[0][0]) &&
+                       (E->sx[m][1][node]< remoteBBox[1][0]) &&
+                       (E->sx[m][2][node]> remoteBBox[0][1]) &&
+                       (E->sx[m][2][node]< remoteBBox[1][1]) &&
+                       (E->sx[m][3][node]> remoteBBox[0][2]) &&
+                       (E->sx[m][3][node]< remoteBBox[1][2]))
                     {
-                        size_++;
-                    }
-
+                       appendX(E, m, node);
+                    }                    
                 }
-    X_.resize(size_);
-     n=0;
-    for (int m=1;m<=E->sphere.caps_per_proc;m++)
-        for(int i=1;i<=E->lmesh.nox;i++)
-	    for(int j=1;j<=E->lmesh.noy;j++)
-		for(int k=1;k<=E->lmesh.noz;k++)
-                {
-                    node = k + (i-1)*E->lmesh.noz+(j-1)*E->lmesh.nox*E->lmesh.noz;
-                    if((E->sx[m][1][node]> b->theta_min()) &&
-                       (E->sx[m][1][node]< b->theta_max()) &&
-                       (E->sx[m][2][node]> b->fi_min()) &&
-                       (E->sx[m][2][node]< b->fi_max()) &&
-                       (E->sx[m][3][node]> b->ri()) &&
-                       (E->sx[m][3][node]< b->ro()))
-                    {
-                        for(l=0;l<DIM;l++) X[k][n] = E->sx[m][l+1][node];
-                        n++;
-                    }
-                }
-#endif
+    
 }
 
 
@@ -75,6 +50,6 @@ void Interior::broadcast(const MPI_Comm& comm, int broadcaster)
 
 
 // version
-// $Id: Interior.cc,v 1.1 2003/11/07 01:08:01 tan2 Exp $
+// $Id: Interior.cc,v 1.2 2003/11/07 05:14:16 puru Exp $
 
 // End of file
