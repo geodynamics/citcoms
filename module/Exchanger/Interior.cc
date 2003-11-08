2d19d314eab6d71f42e76f862d9e3d9afde4e745
Puru Thoutireddy <puru@cacr.caltech.edu>
2003-11-08 04:41:32 +0000

    Made it easier to read and initialized bbox_

--- module/Exchanger/Interior.cc
+++ module/Exchanger/Interior.cc
@@ -23,22 +23,15 @@ Interior::Interior() :
 
 
 Interior::Interior(const BoundedBox& remoteBBox, const All_variables* E) :
-    BoundedMesh(E)
+    BoundedMesh()
 {
+    bbox_ = remoteBBox;
+    
     std::vector<double> x(DIM);
-    int interiornodes,node;
-    interiornodes=0;
-
-    for (int m=1;m<=E->sphere.caps_per_proc;m++)
-        for(int i=1;i<=E->lmesh.nox;i++) 
-	    for(int j=1;j<=E->lmesh.noy;j++)
-		for(int k=1;k<=E->lmesh.noz;k++)   
-                {
-                    interiornodes++;
-                }
-
-    X_.reserve(interiornodes);
-    meshID_.reserve(interiornodes);
+    int node;
+    
+    X_.reserve(E->lmesh.nno);
+    meshID_.reserve(E->lmesh.nno);
     
     for (int m=1;m<=E->sphere.caps_per_proc;m++)
         for(int i=1;i<=E->lmesh.nox;i++) 
@@ -65,15 +58,7 @@ Interior::Interior(const BoundedBox& remoteBBox, const All_variables* E) :
     meshID_.print("meshID");
 }
 
-
-void Interior::broadcast(const MPI_Comm& comm, int broadcaster)
-{
-    // do nothing
-}
-
-
-
 // version
-// $Id: Interior.cc,v 1.3 2003/11/07 05:47:16 puru Exp $
+// $Id: Interior.cc,v 1.4 2003/11/07 20:41:32 puru Exp $
 
 // End of file
