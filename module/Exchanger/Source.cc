c86b68bf1f96deb1bd0addd577f7f1d512ebefd7
Eunseo Choi <echoi@ig.utexas.edu>
2003-11-24 02:11:08 +0000

    a typo fixed

--- module/Exchanger/Source.cc
+++ module/Exchanger/Source.cc
@@ -49,7 +49,7 @@ void Source::interpolateV(Array2D<double,DIM>& V, const All_variables* E) const
 }
 
 
-void Source::interpolateF(Array2D<double,DIM>& V, const All_variables* E) const
+void Source::interpolateF(Array2D<double,DIM>& F, const All_variables* E) const
 {
     if(size())
 	interp->interpolateF(F, E);
@@ -74,6 +74,6 @@ void Source::sendMeshNode() const
 
 
 // version
-// $Id: Source.cc,v 1.2 2003/11/23 18:10:34 ces74 Exp $
+// $Id: Source.cc,v 1.3 2003/11/23 18:11:08 ces74 Exp $
 
 // End of file
