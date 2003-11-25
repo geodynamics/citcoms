466407745cd947c9e227784a4f1bfba37270f7b0
Eunseo Choi <echoi@ig.utexas.edu>
2003-11-25 05:03:02 +0000

    The prototype of "interpolateF" added to the class "Source" as a public member function.

--- module/Exchanger/Source.h
+++ module/Exchanger/Source.h
@@ -38,6 +38,7 @@ public:
     inline int size() const {return meshNode_.size();}
     void interpolateT(Array2D<double,1>& T, const All_variables* E) const;
     void interpolateV(Array2D<double,DIM>& V, const All_variables* E) const;
+    void interpolateF(Array2D<double,DIM>& F, const All_variables* E) const;
 
     template <class T, int N>
     void sendArray2D(const Array2D<T,N>& array) const;
@@ -104,6 +105,6 @@ void Source::sendArray2D(const Array2D<T1,N1>& array1,
 #endif
 
 // version
-// $Id: Source.h,v 1.3 2003/11/11 19:29:27 tan2 Exp $
+// $Id: Source.h,v 1.4 2003/11/24 21:03:02 ces74 Exp $
 
 // End of file
