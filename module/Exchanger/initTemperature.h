1d7c17732318754a7b9cbc2b4395cec7ea909ea1
Eh Tan <tan2@earth.sinica.edu.tw>
2003-12-19 16:05:13 +0000

    add functions initTemperatureSink(), initTemperatureSource() and modifyT() for exchange initial temperature field between CitcomS-CitcomS

--- module/Exchanger/initTemperature.h
+++ module/Exchanger/initTemperature.h
@@ -10,13 +10,24 @@
 #if !defined(pyCitcom_initTemperature_h)
 #define pyCitcom_initTemperature_h
 
-struct All_variables;
+#include "BoundedBox.h"
 
-void initTemperature(All_variables* E);
+struct All_variables;
+class Interior;
+class Sink;
+class Source;
+
+void initTemperatureSink(const Interior& interior,
+			 const Sink& sink,
+			 All_variables* E);
+void initTemperatureSource(const Source& source,
+			   All_variables* E);
+void modifyT(const BoundedBox& bbox, All_variables* E);
+
 
 #endif
 
 // version
-// $Id: initTemperature.h,v 1.1 2003/11/07 01:08:01 tan2 Exp $
+// $Id: initTemperature.h,v 1.2 2003/12/19 08:05:13 tan2 Exp $
 
 // End of file
