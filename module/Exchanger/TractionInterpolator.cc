948751d74c85686c6c01f9d534dd5b251de0da27
Eunseo Choi <echoi@ig.utexas.edu>
2004-01-15 09:04:32 +0000

    1. in initComputeTraction, bbox is now boundedMesh.tightBBox().
    2. domain_cutout() causes a problem in Citcom. Until fixed, it's not called.

--- module/Exchanger/TractionInterpolator.cc
+++ module/Exchanger/TractionInterpolator.cc
@@ -19,9 +19,12 @@ extern "C" {
 
 #include "element_definitions.h"
 
-    void construct_side_c3x3matrix_el(const struct All_variables*,int,struct CC,struct CCX,int,int,int,int,int);
-    void get_global_side_1d_shape_fn(const struct All_variables*,int,struct Shape_function1,struct Shape_function1_dx,struct Shape_function_side_dA,int,int,int);
-
+    void construct_side_c3x3matrix_el(const struct All_variables*,int,struct CC*, struct CCX*,
+				      int,int,int,int,int);
+    void get_global_side_1d_shape_fn(const struct All_variables*,int,struct Shape_function1*,struct Shape_function1_dx*,struct Shape_function_side_dA*,int,int,int);
+    void get_global_shape_fn(const struct All_variables*,int,struct Shape_function*,struct Shape_function_dx*,struct Shape_function_dA*,int,const int,double[4][9],int,int);
+    void velo_from_element(const struct All_variables*,float[4][9],int,int,int);
+    void exchange_node_f(const struct All_variables*,float**,int);
 }
 
 
@@ -37,16 +40,12 @@ TractionInterpolator::TractionInterpolator(const BoundedMesh& boundedMesh,
     initComputeTraction(boundedMesh);
 
     // for the time being, domain_cutout is hidden here.
-    domain_cutout();
-
+    //domain_cutout();
 }
 
 
 TractionInterpolator::~TractionInterpolator()
-{
-    for(int j=0; j<DIM; j++)
-	delete [] gtraction[j];
-}
+{}
 
 
 void TractionInterpolator::interpolateTraction(Array2D<double,DIM>& target)
@@ -54,6 +53,7 @@ void TractionInterpolator::interpolateTraction(Array2D<double,DIM>& target)
     target.assign(size(), 0);
 
     computeTraction();
+    gtraction.print("gtraction");
 
     const int mm = 1;
     for(int i=0; i<size(); i++) {
@@ -82,6 +82,7 @@ void TractionInterpolator::domain_cutout()
 	      << dm_xmin << " " << dm_xmax << " " << dm_ymin << " "
 	      << dm_ymax << " " << dm_zmin << " " << dm_zmax << std::endl;
 
+    std::cout << "lev max=" << E->mesh.levmax << " lev min=" << E->mesh.levmin <<std::endl;
     if(isRelevant) {
 	for(int i=dm_ymin;i<=dm_ymax;i++)
 	    for(int j=dm_xmin;j<=dm_xmax;j++)
@@ -92,14 +93,202 @@ void TractionInterpolator::domain_cutout()
 		    int el=kk+(jj-1)*E->lmesh.elz+(ii-1)*E->lmesh.elz*E->lmesh.elx;
 		    E->element[m][el]=1;
 
-		    std::cout << "Elements cut out: " << el << " "
-			      << k << " " << j << " " << i << std::endl;
+//  		    std::cout << "Elements cut out: " << el << " "
+//  			      << k << " " << j << " " << i << std::endl;
 		}
     }
 }
 
 /////////////////////////////////////////////////////////
 // private functions
+void TractionInterpolator::get_global_stress(const All_variables* E)
+{
+
+    float *SXX[NCS],*SYY[NCS],*SXY[NCS],*SXZ[NCS],*SZY[NCS],*SZZ[NCS];
+    float *divv[NCS],*vorv[NCS];
+    float *H,VV[4][9],Vxyz[9][9],Szz,Sxx,Syy,Sxy,Sxz,Szy,div,vor;
+    float velo_scaling,topo_scaling1,topo_scaling2,stress_scaling;
+    double pre[9],tww[9],rtf[4][9];
+    struct Shape_function GN;
+    struct Shape_function_dA dOmega;
+    struct Shape_function_dx GNx;
+
+    const int dims=E->mesh.nsd;
+    const int vpts=vpoints[dims];
+    const int ends=enodes[dims];
+    const int lev=E->mesh.levmax;
+    const int sphere_key=1;
+
+    H = new float [E->lmesh.noz+1];
+
+    for(int m=1;m<=E->sphere.caps_per_proc;m++)      {
+
+	SXX[m] = new float [E->lmesh.nno+1];
+	SYY[m] = new float [E->lmesh.nno+1];
+	SXY[m] = new float [E->lmesh.nno+1];
+	SXZ[m] = new float [E->lmesh.nno+1];
+	SZY[m] = new float [E->lmesh.nno+1];
+	SZZ[m] = new float [E->lmesh.nno+1];
+	divv[m] = new float [E->lmesh.nno+1];
+	vorv[m] = new float [E->lmesh.nno+1];
+    }
+
+    for(int m=1;m<=E->sphere.caps_per_proc;m++)      {
+	for(int i=1;i<=E->lmesh.nno;i++) {
+	    SZZ[m][i] = 0.0;
+	    SXX[m][i] = 0.0;
+	    SYY[m][i] = 0.0;
+	    SXY[m][i] = 0.0;
+	    SXZ[m][i] = 0.0;
+	    SZY[m][i] = 0.0;
+	    divv[m][i] = 0.0;
+	    vorv[m][i] = 0.0;
+	}
+
+	for(int e=1;e<=E->lmesh.nel;e++)  {
+	    Szz = 0.0;
+	    Sxx = 0.0;
+	    Syy = 0.0;
+	    Sxy = 0.0;
+	    Sxz = 0.0;
+	    Szy = 0.0;
+	    div = 0.0;
+	    vor = 0.0;
+
+	    get_global_shape_fn(E,e,&GN,&GNx,&dOmega,0,sphere_key,rtf,E->mesh.levmax,m);
+	    velo_from_element(E,VV,m,e,sphere_key);
+
+	    for(int j=1;j<=vpts;j++)  {
+		pre[j] =  E->EVi[m][(e-1)*vpts+j]*dOmega.vpt[j];
+		Vxyz[1][j] = 0.0;
+		Vxyz[2][j] = 0.0;
+		Vxyz[3][j] = 0.0;
+		Vxyz[4][j] = 0.0;
+		Vxyz[5][j] = 0.0;
+		Vxyz[6][j] = 0.0;
+		Vxyz[7][j] = 0.0;
+		Vxyz[8][j] = 0.0;
+	    }
+
+	    for(int i=1;i<=ends;i++) {
+		tww[i] = 0.0;
+		for(int j=1;j<=vpts;j++)
+		    tww[i] += dOmega.vpt[j] * g_point[j].weight[E->mesh.nsd-1]
+			* E->N.vpt[GNVINDEX(i,j)];
+         }
+
+	    for(int j=1;j<=vpts;j++)   {
+		for(int i=1;i<=ends;i++)   {
+		    Vxyz[1][j]+=( VV[1][i]*GNx.vpt[GNVXINDEX(0,i,j)]
+				  + VV[3][i]*E->N.vpt[GNVINDEX(i,j)] )*rtf[3][j];
+		    Vxyz[2][j]+=( (VV[2][i]*GNx.vpt[GNVXINDEX(1,i,j)]
+				   + VV[1][i]*E->N.vpt[GNVINDEX(i,j)]*cos(rtf[1][j]))/sin(rtf[1][j])
+				  + VV[3][i]*E->N.vpt[GNVINDEX(i,j)] )*rtf[3][j];
+		    Vxyz[3][j]+= VV[3][i]*GNx.vpt[GNVXINDEX(2,i,j)];
+
+		    Vxyz[4][j]+=( (VV[1][i]*GNx.vpt[GNVXINDEX(1,i,j)]
+				   - VV[2][i]*E->N.vpt[GNVINDEX(i,j)]*cos(rtf[1][j]))/sin(rtf[1][j])
+				  + VV[2][i]*GNx.vpt[GNVXINDEX(0,i,j)])*rtf[3][j];
+		    Vxyz[5][j]+=VV[1][i]*GNx.vpt[GNVXINDEX(2,i,j)] + rtf[3][j]*(VV[3][i]
+										*GNx.vpt[GNVXINDEX(0,i,j)]-VV[1][i]*E->N.vpt[GNVINDEX(i,j)]);
+		    Vxyz[6][j]+=VV[2][i]*GNx.vpt[GNVXINDEX(2,i,j)] + rtf[3][j]*(VV[3][i]
+										*GNx.vpt[GNVXINDEX(1,i,j)]/sin(rtf[1][j])-VV[2][i]*E->N.vpt[GNVINDEX(i,j)]);
+		    Vxyz[7][j]+=rtf[3][j] * (
+					     VV[1][i]*GNx.vpt[GNVXINDEX(0,i,j)]
+					     + VV[1][i]*E->N.vpt[GNVINDEX(i,j)]*cos(rtf[1][j])/sin(rtf[1][j])
+					     + VV[2][i]*GNx.vpt[GNVXINDEX(1,i,j)]/sin(rtf[1][j])  );
+		    Vxyz[8][j]+=rtf[3][j]/sin(rtf[1][j])*
+			( VV[2][i]*GNx.vpt[GNVXINDEX(0,i,j)]*sin(rtf[1][j])
+			  + VV[2][i]*E->N.vpt[GNVINDEX(i,j)]*cos(rtf[1][j])
+			  - VV[1][i]*GNx.vpt[GNVXINDEX(1,i,j)] );
+		}
+		Sxx += 2.0 * pre[j] * Vxyz[1][j];
+		Syy += 2.0 * pre[j] * Vxyz[2][j];
+		Szz += 2.0 * pre[j] * Vxyz[3][j];
+		Sxy += pre[j] * Vxyz[4][j];
+		Sxz += pre[j] * Vxyz[5][j];
+		Szy += pre[j] * Vxyz[6][j];
+		div += Vxyz[7][j]*dOmega.vpt[j];
+		vor += Vxyz[8][j]*dOmega.vpt[j];
+	    }
+
+	    Sxx /= E->eco[m][e].area;
+	    Syy /= E->eco[m][e].area;
+	    Szz /= E->eco[m][e].area;
+	    Sxy /= E->eco[m][e].area;
+	    Sxz /= E->eco[m][e].area;
+	    Szy /= E->eco[m][e].area;
+	    div /= E->eco[m][e].area;
+	    vor /= E->eco[m][e].area;
+
+	    Szz -= E->P[m][e];  /* add the pressure term */
+	    Sxx -= E->P[m][e];  /* add the pressure term */
+	    Syy -= E->P[m][e];  /* add the pressure term */
+
+	    for(int i=1;i<=ends;i++) {
+		int node = E->ien[m][e].node[i];
+		SZZ[m][node] += tww[i] * Szz;
+		SXX[m][node] += tww[i] * Sxx;
+		SYY[m][node] += tww[i] * Syy;
+		SXY[m][node] += tww[i] * Sxy;
+		SXZ[m][node] += tww[i] * Sxz;
+		SZY[m][node] += tww[i] * Szy;
+		divv[m][node]+= tww[i] * div;
+		vorv[m][node]+= tww[i] * vor;
+            }
+
+	}    /* end for el */
+    }     /* end for m */
+
+    exchange_node_f(E,SZZ,lev);
+    exchange_node_f(E,SXX,lev);
+    exchange_node_f(E,SYY,lev);
+    exchange_node_f(E,SXY,lev);
+    exchange_node_f(E,SXZ,lev);
+    exchange_node_f(E,SZY,lev);
+    exchange_node_f(E,divv,lev);
+    exchange_node_f(E,vorv,lev);
+
+    stress_scaling = velo_scaling = topo_scaling1 = topo_scaling2 = 1.0;
+
+    for(int m=1;m<=E->sphere.caps_per_proc;m++)
+	for(int node=1;node<=E->lmesh.nno;node++)   {
+	    SZZ[m][node] = SZZ[m][node]*E->Mass[m][node]*stress_scaling;
+	    SXX[m][node] = SXX[m][node]*E->Mass[m][node]*stress_scaling;
+	    SYY[m][node] = SYY[m][node]*E->Mass[m][node]*stress_scaling;
+	    SXY[m][node] = SXY[m][node]*E->Mass[m][node]*stress_scaling;
+	    SXZ[m][node] = SXZ[m][node]*E->Mass[m][node]*stress_scaling;
+	    SZY[m][node] = SZY[m][node]*E->Mass[m][node]*stress_scaling;
+	    vorv[m][node] = vorv[m][node]*E->Mass[m][node]*velo_scaling;
+	    divv[m][node] = divv[m][node]*E->Mass[m][node]*velo_scaling;
+        }
+
+    /* assign stress to all the nodes */
+    for(int m=1;m<=E->sphere.caps_per_proc;m++)
+	for (int node=1;node<=E->lmesh.nno;node++) {
+	    E->gstress[m][(node-1)*6+1] = SXX[m][node];
+	    E->gstress[m][(node-1)*6+2] = SZZ[m][node];
+	    E->gstress[m][(node-1)*6+3] = SYY[m][node];
+	    E->gstress[m][(node-1)*6+4] = SXY[m][node];
+	    E->gstress[m][(node-1)*6+5] = SXZ[m][node];
+	    E->gstress[m][(node-1)*6+6] = SZY[m][node];
+	}
+
+    for(int m=1;m<=E->sphere.caps_per_proc;m++)        {
+	delete SXX[m];
+	delete SYY[m];
+	delete SXY[m];
+	delete SXZ[m];
+	delete SZY[m];
+	delete SZZ[m];
+	delete divv[m];
+	delete vorv[m];
+    }
+    delete H;
+
+    return;
+}
+
 
 void TractionInterpolator::get_elt_traction(int el,
 					    int far,
@@ -147,15 +336,9 @@ void TractionInterpolator::get_elt_traction(int el,
 	    traction_at_gs[i][j] = 0.0;
 	}
     }
-    // Test
-    std::cout << "get_elt_traction: " << "el=" << el << " "
-	      << "lev=" << lev << " " << "m=" << m << " " << "NS=" << NS
-	      << " " << "far=" << far << std::endl;
-    //
 
-    construct_side_c3x3matrix_el(E,el,Cc,Ccx,lev,m,0,NS,far);
-    get_global_side_1d_shape_fn(E,el,GM,GMx,dGamma,NS,far,m);
-
+    construct_side_c3x3matrix_el(E,el,&Cc,&Ccx,lev,m,0,NS,far);
+    get_global_side_1d_shape_fn(E,el,&GM,&GMx,&dGamma,NS,far,m);
 
     // if normal is in theta direction: 0, in fi: 1, and in r: 2
     if(NS==0)
@@ -179,6 +362,10 @@ void TractionInterpolator::get_elt_traction(int el,
 	    traction[2][j] = E->gstress[m][(a-1)*6+6];
 	    traction[3][j] = E->gstress[m][(a-1)*6+2];
 	}
+    else {
+	std::cout << " NS value is wrong!!" << std::endl;
+	exit(0);
+    }
 
     // seems ad hoc: improve later
     if(far==0 && (NS==0 || NS==1)) {
@@ -228,10 +415,9 @@ void TractionInterpolator::get_elt_traction(int el,
 void TractionInterpolator::initComputeTraction(const BoundedMesh& boundedMesh)
 {
 
-    for(int j=0; j<DIM; j++)
-	gtraction[j] = new float[E->lmesh.nno+1];
+    gtraction.resize(E->lmesh.nno+1);
 
-    const BoundedBox& bbox = boundedMesh.bbox();
+    const BoundedBox bbox = boundedMesh.tightBBox();
 
     int elx=E->lmesh.elx;
     int ely=E->lmesh.ely;
@@ -474,16 +660,18 @@ void TractionInterpolator::computeTraction()
     int mm=1;
     // north/south
 
+    get_global_stress(E);
+
     for(int i=ymin;i<=ymax;i++)
 	for(int j=zmin;j<=elz;j++) {
 	    if(do_xmin) {
 		int el=j+(xmin-1)*elz+(i-1)*elz*elx;
-		std::cout << "N/S:" << el << std::endl;
+// 		std::cout << "N/S:" << el << std::endl;
 		get_elt_traction(el,1,0,lev,mm);
 	    }
 	    if(do_xmax) {
 		int el=j+(xmax-1)*elz+(i-1)*elz*elx;
-		std::cout << "N/S:" << el << std::endl;
+// 		std::cout << "N/S:" << el << std::endl;
 		get_elt_traction(el,0,0,lev,mm);
 	    }
 	}
@@ -492,12 +680,12 @@ void TractionInterpolator::computeTraction()
 	for(int j=zmin;j<=elz;j++) {
 	    if(do_ymin) {
 		int el=j+(i-1)*elz+(ymin-1)*elz*elx;
-		std::cout << "W/E:" << el << std::endl;
+// 		std::cout << "W/E:" << el << std::endl;
 		get_elt_traction(el,1,1,lev,mm);
 	    }
 	    if(do_ymax) {
 		int el=j+(i-1)*elz+(ymax-1)*elz*elx;
-		std::cout << "W/E:" << el << std::endl;
+// 		std::cout << "W/E:" << el << std::endl;
 		get_elt_traction(el,0,1,lev,mm);
 	    }
 	}
@@ -506,7 +694,7 @@ void TractionInterpolator::computeTraction()
 	for(int j=xmin;j<=xmax;j++) {
 	    if(do_zmin) {
 		int el=zmin+(j-1)*elz+(i-1)*elz*elx;
-		std::cout << "B:" << el << std::endl;
+// 		std::cout << "B:" << el << std::endl;
 		get_elt_traction(el,1,2,lev,mm);
 	    }
 	}
@@ -515,6 +703,6 @@ void TractionInterpolator::computeTraction()
 }
 
 // version
-// $Id: TractionInterpolator.cc,v 1.7 2004/01/14 02:40:40 ces74 Exp $
+// $Id: TractionInterpolator.cc,v 1.8 2004/01/15 01:04:32 ces74 Exp $
 
 // End of file
