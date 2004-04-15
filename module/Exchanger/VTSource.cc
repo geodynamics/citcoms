f4f9b8ecd6646f1e9beaf9023114590d71f25b32
Eh Tan <tan2@earth.sinica.edu.tw>
2004-04-15 03:12:13 +0000

    implement interpolateStress()

--- module/Exchanger/VTSource.cc
+++ module/Exchanger/VTSource.cc
@@ -8,11 +8,27 @@
 //
 
 #include <portinfo>
+#include "global_defs.h"
 #include "journal/journal.h"
 #include "BoundedBox.h"
 #include "BoundedMesh.h"
 #include "VTInterpolator.h"
 #include "VTSource.h"
+
+extern "C" {
+    void allocate_STD_mem(const struct All_variables *E,
+			  float** SXX, float** SYY, float** SZZ,
+			  float** SXY, float** SXZ, float** SZY,
+			  float** divv, float** vorv);
+    void free_STD_mem(const struct All_variables *E,
+		      float** SXX, float** SYY, float** SZZ,
+		      float** SXY, float** SXZ, float** SZY,
+		      float** divv, float** vorv);
+    void compute_nodal_stress(const struct All_variables *E,
+			      float** SXX, float** SYY, float** SZZ,
+			      float** SXY, float** SXZ, float** SZY,
+			      float** divv, float** vorv);
+}
 
 
 VTSource::VTSource(MPI_Comm comm, int sink,
@@ -27,6 +43,23 @@ VTSource::VTSource(MPI_Comm comm, int sink,
 
 VTSource::~VTSource()
 {}
+
+
+void VTSource::interpolateStress(Array2D<double,STRESS_DIM>& S) const
+{
+    // copied and modified from get_STD_topo() in Topo_gravity.c
+
+    float *SXX[NCS], *SYY[NCS], *SZZ[NCS];
+    float *SXY[NCS], *SXZ[NCS], *SZY[NCS];
+    float *divv[NCS], *vorv[NCS];
+
+    allocate_STD_mem(E, SXX, SYY, SZZ, SXY, SXZ, SZY, divv, vorv);
+    compute_nodal_stress(E, SXX, SYY, SZZ, SXY, SXZ, SZY, divv, vorv);
+    free_STD_mem(E, SXX, SYY, SZZ, SXY, SXZ, SZY, divv, vorv);
+
+    if(size())
+	interp->interpolateStress(S);
+}
 
 
 void VTSource::interpolateTemperature(Array2D<double,1>& T) const
@@ -52,6 +85,6 @@ void VTSource::createInterpolator(const BoundedMesh& mesh)
 
 
 // version
-// $Id: VTSource.cc,v 1.2 2004/02/25 23:07:35 tan2 Exp $
+// $Id: VTSource.cc,v 1.3 2004/04/14 20:12:13 tan2 Exp $
 
 // End of file
--- module/Exchanger/VTSource.h
+++ module/Exchanger/VTSource.h
@@ -32,23 +32,17 @@ public:
 	     const BoundedBox& mybbox);
     virtual ~VTSource();
 
+    virtual void interpolateStress(Array2D<double,STRESS_DIM>& S) const;
     virtual void interpolateTemperature(Array2D<double,1>& T) const;
     virtual void interpolateVelocity(Array2D<double,DIM>& V) const;
 
 private:
     virtual void createInterpolator(const BoundedMesh& mesh);
 
-#if 0
-    void recvMesh(BoundedMesh& mesh);
-    void sendMeshNode() const;
-    void initX(const BoundedMesh& mesh);
-#endif
-
     // disable these functions
     virtual void interpolateDisplacement(Array2D<double,DIM>& D) const {};
     virtual void interpolateForce(Array2D<double,DIM>& F) const {};
     virtual void interpolatePressure(Array2D<double,1>& P) const {};
-    virtual void interpolateStress(Array2D<double,STRESS_DIM>& S) const {};
     virtual void interpolateTraction(Array2D<double,DIM>& F) const {};
 
     // disable copy c'tor and assignment operator
@@ -61,6 +55,6 @@ private:
 #endif
 
 // version
-// $Id: VTSource.h,v 1.3 2004/04/14 19:41:38 tan2 Exp $
+// $Id: VTSource.h,v 1.4 2004/04/14 20:12:13 tan2 Exp $
 
 // End of file
