21873a1d840b2dede78f3dd803b7c478b2990b59
Eh Tan <tan2@earth.sinica.edu.tw>
2005-01-19 07:19:07 +0000

    uncommented tracer module

--- drivers/Citcom.c
+++ drivers/Citcom.c
@@ -86,11 +86,10 @@ int main(argc,argv)
 
 
   if (E->control.stokes)  {
-    /*      if(E->control.tracer==1)  {
-	    (E->problem_tracer_advection)(E);
-	    (E->problem_tracer_output)(E,E->monitor.solution_cycles);
-	    }
-    */
+    if(E->control.tracer==1)  {
+      (E->problem_tracer_advection)(E);
+      (E->problem_tracer_output)(E,E->monitor.solution_cycles);
+    }
     parallel_process_termination();
   }
 
@@ -120,12 +119,10 @@ int main(argc,argv)
 
     general_stokes_solver(E);
 
-
-    /*      if(E->control.tracer==1)  {
-	    (E->problem_tracer_advection)(E);
-	    (E->problem_tracer_output)(E,E->monitor.solution_cycles);
-	    }
-    */
+    if(E->control.tracer==1)  {
+      (E->problem_tracer_advection)(E);
+      (E->problem_tracer_output)(E,E->monitor.solution_cycles);
+    }
 
     if ((E->monitor.solution_cycles % E->control.record_every)==0) {
       if(E->control.pseudo_free_surf) {
