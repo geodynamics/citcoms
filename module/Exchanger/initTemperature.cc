9a03edab19e4b4ec191010762282dffbd0947290
Eunseo Choi <echoi@ig.utexas.edu>
2005-05-20 05:01:45 +0000

    * added a new function for the initial temperature field. 	- called "hot_blob_lith" 	- Thermal boudnary layer on top + one hot bloc at the center.

--- module/Exchanger/initTemperature.cc
+++ module/Exchanger/initTemperature.cc
@@ -24,7 +24,11 @@ void plate(const BoundedBox& bbox, All_variables* E);
 void hot_blob(const BoundedBox& bbox, All_variables* E);
 void hot_blob_below(const BoundedBox& bbox, All_variables* E);
 void five_hot_blobs(const BoundedBox& bbox, All_variables* E);
+void hot_blob_lith(const BoundedBox& bbox, All_variables* E);
 void add_hot_blob(All_variables* E,
+		  double x_center, double y_center, double z_center,
+		  double radius, double baseline, double amp);
+void add_hot_blob_lith(All_variables* E,
 		  double x_center, double y_center, double z_center,
 		  double radius, double baseline, double amp);
 void debug_output(const All_variables* E);
@@ -42,6 +46,7 @@ void initTemperature(const BoundedBox& bbox, All_variables* E)
     //hot_blob_below(bbox, E);
     //hot_blob(bbox, E);
     //five_hot_blobs(bbox, E);
+	//hot_blob_lith(bbox, E);
 
     debug_output(E);
     (E->temperatures_conform_bcs)(E);
@@ -225,7 +230,98 @@ void five_hot_blobs(const BoundedBox& bbox, All_variables* E)
 }
 
 
+void hot_blob_lith(const BoundedBox& bbox, All_variables* E)
+{
+    // put a hot blob in the center bbox and put a LITH_AGE-old lithosphere on top
+    const int noy=E->lmesh.noy;
+    const int nox=E->lmesh.nox;
+    const int noz=E->lmesh.noz;
+	const double LITH_AGE = 100.0; // Myrs
+
+	// set up a thermal boundary layer first
+	if(E->control.lith_age) {
+		for(int m=1;m<=E->sphere.caps_per_proc;m++)
+			for(int i=1;i<=noy;i++)
+				for(int j=1;j<=nox;j++)
+					for(int k=1;k<=noz;k++) {
+						int node=k+(j-1)*noz+(i-1)*nox*noz;
+						int nodet=j+(i-1)*nox;
+						double r1=E->sx[m][3][node];
+						double temp = 0.2*(E->sphere.ro-r1) * 0.5/sqrt(E->age_t[nodet]/E->data.scalet/E->data.scalet);
+						E->T[m][node] = E->control.TBCbotval*erf(temp);
+					}
+	}
+	else {
+		for(int m=1;m<=E->sphere.caps_per_proc;m++)
+			for(int i=1;i<=noy;i++)
+				for(int j=1;j<=nox;j++)
+					for(int k=1;k<=noz;k++) {
+						int node=k+(j-1)*noz+(i-1)*nox*noz;
+						double r1=E->sx[m][3][node];
+						/* THIS PART puts a LITH_AGE-old lithosphere on top */
+						double temp = 1.2*(E->sphere.ro-r1) * 0.5/sqrt(LITH_AGE/E->data.scalet);
+						E->T[m][node] = E->control.TBCbotval*erf(temp);
+					}
+	}
+
+    const double theta_min = E->control.theta_min;
+    const double theta_max = E->control.theta_max;
+    const double fi_min = E->control.fi_min;
+    const double fi_max = E->control.fi_max;
+    const double ri = E->sphere.ri;
+    const double ro = E->sphere.ro;
+
+    // radius of the blob is one third of the smallest dimension
+    double d = std::min(std::min(theta_max - theta_min, fi_max - fi_min), (ro-ri)/3.0);
+
+    // center of fine grid mesh
+    double theta_center = 0.5 * (theta_max + theta_min);
+    double fi_center = 0.5 * (fi_max + fi_min);
+    double r_center = 0.6 * ro +  0.4 * ri;
+    fprintf(stderr,"center=%e %e %e d=%e fi=%e %e r=%e %e\n",theta_center,fi_center,r_center,d,fi_max,fi_min,ro,ri);
+
+    double x_center = r_center * sin(fi_center) * cos(theta_center);
+    double y_center = r_center * sin(fi_center) * sin(theta_center);
+    double z_center = r_center * cos(fi_center);
+
+    // compute temperature field according to nodal coordinate
+    add_hot_blob_lith(E, x_center, y_center, z_center, d, E->control.lith_age_mantle_temp, 1.0-E->control.lith_age_mantle_temp);
+}
+
+
 void add_hot_blob(All_variables* E,
+		  double x_center, double y_center, double z_center,
+		  double radius, double baseline, double amp)
+{
+    // compute temperature field according to nodal coordinate
+    for(int m=1;m<=E->sphere.caps_per_proc;m++)
+        for(int k=1;k<=E->lmesh.noy;k++)
+            for(int j=1;j<=E->lmesh.nox;j++)
+                for(int i=1;i<=E->lmesh.noz;i++)  {
+                    int node = i + (j-1)*E->lmesh.noz
+                             + (k-1)*E->lmesh.noz*E->lmesh.nox;
+
+                    double theta = E->sx[m][1][node];
+                    double fi = E->sx[m][2][node];
+                    double r = E->sx[m][3][node];
+
+                    double x = r * sin(fi) * cos(theta);
+                    double y = r * sin(fi) * sin(theta);
+                    double z = r * cos(fi);
+
+                    double distance = sqrt((x - x_center)*(x - x_center) +
+                                           (y - y_center)*(y - y_center) +
+                                           (z - z_center)*(z - z_center));
+
+                    if (distance < radius)
+                         E->T[m][node] += baseline
+                                     + amp * cos(distance/radius * M_PI);
+
+                }
+}
+
+
+void add_hot_blob_lith(All_variables* E,
 		  double x_center, double y_center, double z_center,
 		  double radius, double baseline, double amp)
 {
@@ -250,8 +346,7 @@ void add_hot_blob(All_variables* E,
                                            (z - z_center)*(z - z_center));
 
                     if (distance < radius)
-                        E->T[m][node] += baseline
-			              + amp * cos(distance/radius * M_PI);
+                        E->T[m][node] += amp * exp(-1.0*distance/radius);
 
                 }
 }
@@ -380,6 +475,6 @@ void basal_tbl_central_hot_blob(const BoundedBox& bbox, All_variables* E)
 
 
 // version
-// $Id: initTemperature.cc,v 1.6 2004/10/08 00:11:23 tan2 Exp $
+// $Id: initTemperature.cc,v 1.7 2005/05/19 22:01:45 ces74 Exp $
 
 // End of file
