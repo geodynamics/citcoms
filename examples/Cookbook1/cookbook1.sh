bd418e429fb7d71bd4338f8667b7068d2d95386d
Michael Gurnis <gurnis@caltech.edu>
2005-05-28 04:11:59 +0000

    *** empty log message ***

--- examples/Cookbook1/cookbook1.sh
+++ examples/Cookbook1/cookbook1.sh
@@ -11,18 +11,16 @@
 \
 --staging.nodes=12 \
 --staging.nodegen="n%03d" \
---staging.nodelist=[101-106,101-106] \
+--staging.nodelist=[141-146,141-146] \
 \
 --steps=101 \
 --controller.monitoringFrequency=25 \
 \
---solver.datafile="/scratch/username/cookbook1" \
+--solver.datafile="/scratch/GURNIS/Test/cookbook1" \
 \
 --solver.ic.num_perturbations=1 \
---solver.ic.perturbl=8 \
---solver.ic.perturbm=8 
-
-# version
-# $Id: cookbook1.sh,v 1.1 2004/06/29 17:25:12 tan2 Exp $
-
+--solver.ic.perturbl=3 \
+--solver.ic.perturbm=2 \
+\
+--solver.rayleigh=5.0e+4
 # End of file
