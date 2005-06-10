c56cbaa805f29d8fe28747702533af46f27b8241
Leif Strand <leif@geodynamics.org>
2005-06-10 09:23:11 +0000

    Added license text.

--- module/Regional/mesher.cc
+++ module/Regional/mesher.cc
@@ -2,7 +2,51 @@
 //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-//  <LicenseText>
+//<LicenseText>
+//=====================================================================
+//
+//                             CitcomS.py
+//                 ---------------------------------
+//
+//                              Authors:
+//            Eh Tan, Eun-seo Choi, and Pururav Thoutireddy 
+//          (c) California Institute of Technology 2002-2005
+//
+//        By downloading and/or installing this software you have
+//       agreed to the CitcomS.py-LICENSE bundled with this software.
+//             Free for non-commercial academic research ONLY.
+//      This program is distributed WITHOUT ANY WARRANTY whatsoever.
+//
+//=====================================================================
+//
+//  Copyright June 2005, by the California Institute of Technology.
+//  ALL RIGHTS RESERVED. United States Government Sponsorship Acknowledged.
+// 
+//  Any commercial use must be negotiated with the Office of Technology
+//  Transfer at the California Institute of Technology. This software
+//  may be subject to U.S. export control laws and regulations. By
+//  accepting this software, the user agrees to comply with all
+//  applicable U.S. export laws and regulations, including the
+//  International Traffic and Arms Regulations, 22 C.F.R. 120-130 and
+//  the Export Administration Regulations, 15 C.F.R. 730-744. User has
+//  the responsibility to obtain export licenses, or other export
+//  authority as may be required before exporting such information to
+//  foreign countries or providing access to foreign nationals.  In no
+//  event shall the California Institute of Technology be liable to any
+//  party for direct, indirect, special, incidental or consequential
+//  damages, including lost profits, arising out of the use of this
+//  software and its documentation, even if the California Institute of
+//  Technology has been advised of the possibility of such damage.
+// 
+//  The California Institute of Technology specifically disclaims any
+//  warranties, including the implied warranties or merchantability and
+//  fitness for a particular purpose. The software and documentation
+//  provided hereunder is on an "as is" basis, and the California
+//  Institute of Technology has no obligations to provide maintenance,
+//  support, updates, enhancements or modifications.
+//
+//=====================================================================
+//</LicenseText>
 //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
@@ -166,6 +210,6 @@ PyObject * pyCitcom_regional_sphere_launch(PyObject *self, PyObject *args)
 
 
 // version
-// $Id: mesher.cc,v 1.15 2005/01/19 00:38:49 tan2 Exp $
+// $Id: mesher.cc,v 1.16 2005/06/10 02:23:19 leif Exp $
 
 // End of file
--- tests/citcomsregional.sh
+++ tests/citcomsregional.sh
@@ -1,7 +1,51 @@
 #!/bin/sh
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# <LicenseText>
+#<LicenseText>
+#=====================================================================
+#
+#                             CitcomS.py
+#                 ---------------------------------
+#
+#                              Authors:
+#            Eh Tan, Eun-seo Choi, and Pururav Thoutireddy 
+#          (c) California Institute of Technology 2002-2005
+#
+#        By downloading and/or installing this software you have
+#       agreed to the CitcomS.py-LICENSE bundled with this software.
+#             Free for non-commercial academic research ONLY.
+#      This program is distributed WITHOUT ANY WARRANTY whatsoever.
+#
+#=====================================================================
+#
+#  Copyright June 2005, by the California Institute of Technology.
+#  ALL RIGHTS RESERVED. United States Government Sponsorship Acknowledged.
+# 
+#  Any commercial use must be negotiated with the Office of Technology
+#  Transfer at the California Institute of Technology. This software
+#  may be subject to U.S. export control laws and regulations. By
+#  accepting this software, the user agrees to comply with all
+#  applicable U.S. export laws and regulations, including the
+#  International Traffic and Arms Regulations, 22 C.F.R. 120-130 and
+#  the Export Administration Regulations, 15 C.F.R. 730-744. User has
+#  the responsibility to obtain export licenses, or other export
+#  authority as may be required before exporting such information to
+#  foreign countries or providing access to foreign nationals.  In no
+#  event shall the California Institute of Technology be liable to any
+#  party for direct, indirect, special, incidental or consequential
+#  damages, including lost profits, arising out of the use of this
+#  software and its documentation, even if the California Institute of
+#  Technology has been advised of the possibility of such damage.
+# 
+#  The California Institute of Technology specifically disclaims any
+#  warranties, including the implied warranties or merchantability and
+#  fitness for a particular purpose. The software and documentation
+#  provided hereunder is on an "as is" basis, and the California
+#  Institute of Technology has no obligations to provide maintenance,
+#  support, updates, enhancements or modifications.
+#
+#=====================================================================
+#</LicenseText>
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
@@ -10,7 +54,7 @@ mpipython.exe ${EXPORT_ROOT}/modules/CitcomS/SimpleApp.pyc $*
 
 
 # version
-# $Id: citcomsregional.sh,v 1.1 2004/04/23 22:15:42 tan2 Exp $
+# $Id: citcomsregional.sh,v 1.2 2005/06/10 02:23:24 leif Exp $
 
 # End of file
 
