7070376f7f54eca4b45c5482b80627d85dbfead9
Leif Strand <leif@geodynamics.org>
2005-08-23 03:42:05 +0000

    [vcp] create branch 'CitcomS-2_x'

---
+++ module/Full/Make.mm
@@ -0,0 +1,95 @@
+# -*- Makefile -*-
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#<LicenseText>
+#=====================================================================
+#
+#                             CitcomS.py
+#                 ---------------------------------
+#
+#                              Authors:
+#            Eh Tan, Eun-seo Choi, and Pururav Thoutireddy 
+#          (c) California Institute of Technology 2002-2005
+#
+#        By downloading and/or installing this software you have
+#       agreed to the CitcomS.py-LICENSE bundled with this software.
+#             Free for non-commercial academic research ONLY.
+#      This program is distributed WITHOUT ANY WARRANTY whatsoever.
+#
+#=====================================================================
+#
+#  Copyright June 2005, by the California Institute of Technology.
+#  ALL RIGHTS RESERVED. United States Government Sponsorship Acknowledged.
+# 
+#  Any commercial use must be negotiated with the Office of Technology
+#  Transfer at the California Institute of Technology. This software
+#  may be subject to U.S. export control laws and regulations. By
+#  accepting this software, the user agrees to comply with all
+#  applicable U.S. export laws and regulations, including the
+#  International Traffic and Arms Regulations, 22 C.F.R. 120-130 and
+#  the Export Administration Regulations, 15 C.F.R. 730-744. User has
+#  the responsibility to obtain export licenses, or other export
+#  authority as may be required before exporting such information to
+#  foreign countries or providing access to foreign nationals.  In no
+#  event shall the California Institute of Technology be liable to any
+#  party for direct, indirect, special, incidental or consequential
+#  damages, including lost profits, arising out of the use of this
+#  software and its documentation, even if the California Institute of
+#  Technology has been advised of the possibility of such damage.
+# 
+#  The California Institute of Technology specifically disclaims any
+#  warranties, including the implied warranties or merchantability and
+#  fitness for a particular purpose. The software and documentation
+#  provided hereunder is on an "as is" basis, and the California
+#  Institute of Technology has no obligations to provide maintenance,
+#  support, updates, enhancements or modifications.
+#
+#=====================================================================
+#</LicenseText>
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+include local.def
+TYPE=Full
+
+PROJECT = CitcomS
+PACKAGE = $(TYPE)module
+include std-pythonmodule.def
+
+PROJ_CXX_SRCLIB = \
+	-Xlinker -rpath -Xlinker $(PROJ_LIBDIR) \
+	-Xlinker -rpath -Xlinker $(PYTHIA_LIBDIR) \
+	-l$(PROJECT)Common \
+	-l$(PROJECT)$(TYPE) \
+	-ljournal \
+	$(PYTHIA_DIR)/modules/mpi/_mpimodule.so
+
+PROJ_CXX_INCLUDES = ../../lib/Common
+EXTERNAL_INCLUDES += $(PYTHIA_DIR)/include $(PYTHIA_INCDIR)
+EXTERNAL_LIBPATH += -L$(PYTHIA_DIR)/lib -L$(PYTHIA_LIBDIR)
+
+PROJ_SRCS = \
+    advdiffu.cc \
+    bindings.cc \
+    exceptions.cc \
+    initial_conditions.cc \
+    mesher.cc \
+    misc.cc \
+    outputs.cc \
+    setProperties.cc \
+    stokes_solver.cc
+
+
+#===========================================================================
+# link source files from ../Regional to .
+
+link:
+	(cd ../Regional;\
+	 ln -f $(PROJ_SRCS) *.h ../$(TYPE))
+
+# version
+# $Id: Make.mm,v 1.18 2005/06/14 16:47:37 leif Exp $
+
+# End of file
