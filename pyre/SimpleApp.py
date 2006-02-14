1be179ac266d41cafab18c5ebe86be04cdeba181
Leif Strand <leif@geodynamics.org>
2006-02-14 22:24:53 +0000

    Merged r2600 (fix for issue13, "make PYTHONPATH automatic") from v2 branch to trunk.

--- pyre/SimpleApp.py
+++ pyre/SimpleApp.py
@@ -27,6 +27,14 @@
 #
 
 
+# re-create the PYTHONPATH at 'configure' time
+import sys
+from config import makefile
+path = makefile['PYTHONPATH'].split(':')
+for dir in path:
+    sys.path.insert(1, dir)
+
+
 from mpi.Application import Application
 import journal
 
