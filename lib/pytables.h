251a159698ff451a7ebec5861b7aebcedac47d24
Luis Armendariz <luis@geodynamics.org>
2006-08-01 23:17:29 +0000

    Updated prototypes for the pytables-derived functions.

--- lib/pytables.h
+++ lib/pytables.h
@@ -38,20 +38,10 @@ extern "C" {
 /* hardcoded pickle of Filter class */
 #define FILTERS_P "ccopy_reg\n_reconstructor\np1\n(ctables.Leaf\nFilters\np2\nc__builtin__\nobject\np3\nNtRp4\n(dp5\nS'shuffle'\np6\nI0\nsS'complevel'\np7\nI0\nsS'fletcher32'\np8\nI0\nsS'complib'\np9\nS'zlib'\np10\nsb."
 
-
+/* PyTables.c */
 herr_t find_attribute(hid_t loc_id, const char *attr_name);
-
-herr_t set_attribute_string(hid_t obj_id,
-                            const char *attr_name,
-                            const char *attr_data);
-
-herr_t make_array(hid_t loc_id,
-                  const char *dset_name,
-                  const int rank,
-                  const hsize_t *dims,
-                  hid_t type_id,
-                  const void *data);
-
+herr_t set_attribute(hid_t obj_id, const char *attr_name, const char *attr_data);
+herr_t make_array(hid_t loc_id, const char *dset_name, const int rank, const hsize_t *dims, hid_t type_id, const void *data);
 
 
 #ifdef __cplusplus
