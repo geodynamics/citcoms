4bd3a7560db24d525063cb32e5fa06691c483c3a
Leif Strand <leif@geodynamics.org>
2006-11-11 03:34:08 +0000

    Use the new merlin (the setuptools clone).  Download Python dependencies automatically during 'configure'  (This doesn't work for "make install" yet.)
    The Dynamo BuildBot might even succeed after this check-in...
    fingers crossed...

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 
-from ez_setup import use_setuptools
-use_setuptools()
+from archimedes import use_merlin
+use_merlin()
 
-from setuptools import setup, find_packages
+from merlin import setup, find_packages
 
 setup(
     
@@ -12,22 +12,15 @@ setup(
     zip_safe = False,
     packages = find_packages(),
     
-    #setup_requires = [
-    #'merlin',
-    #],
     install_requires = [
-    'pythia[mpi] >= 0.8.1.0b2, < 0.8.2a, == dev',
+    'pythia[mpi] >= 0.8.1.0b3, < 0.8.2a',
     ],
-    extras_require = {
-    'Exchanger': ['Exchanger >= 1.0, < 2.0a'],
-    },
 
     author = 'Louis Moresi, et al.',
     author_email = 'cig-mc@geodynamics.org',
     description = """A finite element mantle convection code.""",
     long_description = """CitcomS is a finite element code designed to solve thermal convection problems relevant to Earth's mantle. Written in C, the code runs on a variety of parallel processing computers, including shared and distributed memory platforms.""",
     license = 'GPL',
-    url = 'http://www.geodynamics.org/cig/software/packages/citcoms/',
-    download_url = 'http://crust.geodynamics.org/~leif/shipping/', # temporary
+    url = 'http://www.geodynamics.org/cig/software/packages/mc/citcoms/',
 
 )
