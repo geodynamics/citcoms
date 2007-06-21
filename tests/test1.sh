3d9403f9aab480d88b7d85748819cc0e8fa21554
Eh Tan <tan2@earth.sinica.edu.tw>
2007-06-21 22:22:08 +0000

    Preparing a conflict test, again

--- tests/test1.sh
+++ tests/test1.sh
@@ -26,6 +26,8 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+#
+#
 # functional test of exchanger
 # 12-proc coarse-grid exchanger, 1-proc fine-grid exchanger
 #
