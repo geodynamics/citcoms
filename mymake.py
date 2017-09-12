758c4f0713feb04c2680475e5476dcc2ec5544b0
Bower Daniel James (bower) <bower@submit01.ubelix.unibe.ch>
2017-09-12 09:03:08 +0200

    simple python make script

---
+++ mymake.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import subprocess
+
+# generate configure and run
+subprocess.call( ['libtoolize','--force'] )
+subprocess.call( ['aclocal'] )
+subprocess.call( ['autoheader'] )
+subprocess.call( ['automake', '--force-missing', '--add-missing'] )
+subprocess.call( ['autoconf'] )
+# I don't know why, but autoreconf -i prevents an error from occuring
+subprocess.call( ['autoreconf', '-i'] )
+subprocess.call( ['./configure'] )
+subprocess.call( ['make'] )
