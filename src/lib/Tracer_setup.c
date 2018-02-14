9a7e7b3ef0668d5a4f2ce0e527623fc881a67e6b
Dan J Bower <djbower@gmail.com>
2018-02-14 13:51:38 +0100

    prevent exit just because tracer count is off (usually by <10 tracers)

--- src/lib/Tracer_setup.c
+++ src/lib/Tracer_setup.c
@@ -1031,11 +1031,13 @@ static void read_tracer_file(struct All_variables *E)
     icheck=isum_tracers(E);
 
     if (icheck!=number_of_tracers) {
-        fprintf(E->trace.fpt,"ERROR(read_tracer_file) - tracers != number in file\n");
+        fprintf(E->trace.fpt,"WARNING(read_tracer_file) - tracers != number in file\n");
         fprintf(E->trace.fpt,"Tracers in system: %d\n", icheck);
         fprintf(E->trace.fpt,"Tracers in file: %d\n", number_of_tracers);
         fflush(E->trace.fpt);
-        exit(10);
+        /* DJB COMP */
+        /* sometimes a handful of tracers go missing */
+        /* exit(10); */
     }
 
     return;
