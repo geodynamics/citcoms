23e96dde72daac579beae1a9e1b72017d6d4ece1
Dan J Bower <djbower@gmail.com>
2018-10-26 13:44:40 +0200

    add ivels

--- src/lib/Full_read_input_from_files.c
+++ src/lib/Full_read_input_from_files.c
@@ -50,6 +50,7 @@ void full_read_input_files_for_timesteps(E,action,output)
     float *TB1, *TB2, *VB1[4],*VB2[4], inputage1, inputage2;
     int nox,noz,noy,nnn,nox1,noz1,noy1;
     int i,ii,ll,m,mm,j,k,n,nodeg,nodel,node,cap;
+    int mm1, mm2; // DJB SLAB
     int intage, pos_age;
     int nodea;
     int nn,el;
@@ -243,6 +244,37 @@ void full_read_input_files_for_timesteps(E,action,output)
           else
              fprintf(E->fp,"Slab Assimilation: File2 = No file inputted (negative age)\n");
         }
+
+        break;
+
+      case 7:  /* read internal velocity */
+        /* DJB SLAB */
+        if( E->parallel.me == 0)
+            fprintf(stderr, "\nInternal Velocity, action=%d output=%d\n",action,output);
+
+        sprintf(output_file1,"%s%0.0f.%d",E->control.velocity_internal_file,newage1,cap);
+        sprintf(output_file2,"%s%0.0f.%d",E->control.velocity_internal_file,newage2,cap);
+        fprintf(stderr, "\nInternal Velocity, %s\n",output_file1);
+        fp1=fopen(output_file1,"r");
+        if (fp1 == NULL) {
+          fprintf(E->fp,"(Problem_related #14) Cannot open %s\n",output_file1);
+          exit(8);
+        }   
+        if (pos_age) {
+           fp2=fopen(output_file2,"r");
+           if (fp2 == NULL) {
+             fprintf(E->fp,"(Problem_related #15) Cannot open %s\n",output_file2);
+             exit(8);
+           }   
+        }   
+        if((E->parallel.me==0) && (output==1))   { 
+           fprintf(E->fp,"Internal Velocity: Starting Age = %g, Elapsed time = %g, Current Age = %g\n",E->control.start_age,E->monitor.elapsed_time,age);
+           fprintf(E->fp,"Internal Velocity: File1 = %s\n",output_file1);
+          if (pos_age)
+             fprintf(E->fp,"Internal Velocity: File2 = %s\n",output_file2);
+          else
+             fprintf(E->fp,"Internal Velocity: File2 = No file inputted (negative age)\n");
+        }   
 
         break;
 
@@ -504,6 +536,98 @@ void full_read_input_files_for_timesteps(E,action,output)
           free ((void *) SS2);
 
         break;
+
+  case 7: /* velocity assimilation */
+
+      /* XXX DJB */
+      nnn=nox*noy*noz;
+      for(i=1;i<=dims+1;i++)  {
+        VB1[i]=(float*) malloc ((nnn+1)*sizeof(float));
+        VB2[i]=(float*) malloc ((nnn+1)*sizeof(float));
+      }
+
+      for(i=1;i<=nnn;i++)   {
+         if(fscanf(fp1,"%f %f %f %f",&(VB1[1][i]),&(VB1[2][i]),&(VB1[3][i]),&(VB1[4][i])) != 4) {
+           fprintf(stderr,"Error while reading file '%s'\n",output_file1);
+           exit(8);
+         }
+         VB1[1][i]=E->data.timedir*VB1[1][i];
+         VB1[2][i]=E->data.timedir*VB1[2][i];
+         VB1[3][i]=E->data.timedir*VB1[3][i];
+         if (pos_age) {
+             if(fscanf(fp2,"%f %f %f %f",&(VB2[1][i]),&(VB2[2][i]),&(VB2[3][i]),&(VB2[4][i])) != 4) {
+                 fprintf(stderr,"Error while reading file '%s'\n",output_file2);
+                 exit(8);
+             }
+             VB2[1][i]=E->data.timedir*VB2[1][i];
+             VB2[2][i]=E->data.timedir*VB2[2][i];
+             VB2[3][i]=E->data.timedir*VB2[3][i];
+         }
+      }
+      fclose(fp1);
+      if (pos_age) fclose(fp2);
+
+      for(j=1;j<=noy1;j++)
+        for(i=1;i<=nox1;i++)
+          for(k=1;k<=noz1;k++)    {
+            nodel = k + (i-1)*noz1 + (j-1)*nox1*noz1;
+            nodeg = (E->lmesh.nzs+k-1) + (E->lmesh.nxs+i-2)*noz + (E->lmesh.nys+j-2)*noz*nox;
+
+            /* flag==2 means skip this node - required for upper and lower surface (all models)
+               to avoid conflict with plate velocities and free slip CMB
+               also required for side boundaries for regional models */
+
+            /* no need to also evaluate VB2 since VB1 and VB2 are both 2 at the same nodes */
+            if ((int)VB1[4][nodeg]!=2) { // && (int)VB2[4][nodeg]!=2) {
+              if (pos_age) {
+                /* age is closest to newage1 */
+                if (abs(age-newage1) <= abs(age-newage2)) {
+                  E->sphere.cap[m].VB[1][nodel] = VB1[1][nodeg]*E->data.scalev;
+                  E->sphere.cap[m].VB[2][nodel] = VB1[2][nodeg]*E->data.scalev;
+                  E->sphere.cap[m].VB[3][nodel] = VB1[3][nodeg]*E->data.scalev;
+                  E->sphere.cap[m].slab_sten2[nodel] = (int)(VB1[4][nodeg]);
+                }
+                /* age is closest to newage2 */
+                else {
+                  E->sphere.cap[m].VB[1][nodel] = VB2[1][nodeg]*E->data.scalev;
+                  E->sphere.cap[m].VB[2][nodel] = VB2[2][nodeg]*E->data.scalev;
+                  E->sphere.cap[m].VB[3][nodel] = VB2[3][nodeg]*E->data.scalev;
+                  E->sphere.cap[m].slab_sten2[nodel] = (int)(VB2[4][nodeg]);
+                }
+              }
+              else { /* negative ages - don't do the interpolation */
+                E->sphere.cap[m].VB[1][nodel] = VB1[1][nodeg]*E->data.scalev;
+                E->sphere.cap[m].VB[2][nodel] = VB1[2][nodeg]*E->data.scalev;
+                E->sphere.cap[m].VB[3][nodel] = VB1[3][nodeg]*E->data.scalev;
+                E->sphere.cap[m].slab_sten2[nodel] = (int)VB1[4][nodeg];
+              }
+
+              /* for debugging */
+              /* fprintf(stderr,"1 %d %d %d %d %d %f %f %f %f\n",noy1,nox1,noz1,nodel,nodeg,VB1[1][nodeg],VB1[2][nodeg],VB1[3][nodeg],VB1[4][nodeg]);
+              fprintf(stderr,"2 %d %d %f %f %f %d\n",nodel,nodeg,E->sphere.cap[1].VB[1][nodel],E->sphere.cap[1].VB[2][nodel],E->sphere.cap[1].VB[3][nodel],E->sphere.cap[1].slab_sten2[nodel]);
+              if((int)VB1[4][nodeg]==1) {
+                fprintf(stderr,"1 flag is ON\n");
+              }
+              if(E->sphere.cap[1].slab_sten2[nodel]==1) {
+                fprintf(stderr,"2 flag is ON\n");
+              } */
+
+            }
+
+            /* else skip this node (do not update velocities) */
+            /* but need stencil value for flags!!! */
+            else {
+              E->sphere.cap[m].slab_sten2[nodel] = (int)VB1[4][nodeg];
+            }
+
+      }
+
+      for(i=1;i<=dims+1;i++) {
+          free ((void *) VB1[i]);
+          free ((void *) VB2[i]);
+      }
+
+      break;
 
       } /* end switch */
     } /* end for m */
