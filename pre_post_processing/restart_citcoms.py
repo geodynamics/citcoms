1f7d8aa42fe807d584ccfb9f945d45ea39fdaa63
Roberta <rcarluccio@student.unimelb.edu.au>
2022-03-31 15:27:34 +1100

    Fixes for restart ic_dir pre_post_processing

--- pre_post_processing/restart_citcoms.py
+++ pre_post_processing/restart_citcoms.py
@@ -442,8 +442,8 @@ def create_no_lith_temp(control_d, master_run_d, rs_replace_d, rs_dir, rs_inp_cf
     Core_Util.make_dir( ic_dir )
     
     # make the output name in ic_dir consistent with routine for Data output - RC
-    out_name = ic_dir + '/' + datafile + '.velo.#.'  + str(0)
     #out_name = ic_dir + '/' + rs_datafile + '.velo.#.' + str(timestep)
+    out_name = ic_dir + '/' + datafile + '.velo.#.'  + str(0)
     print( now(), 'create_no_lith_temp: out_name =', out_name )
     
     # now write out data to processor files (with header, necessary for restart)
